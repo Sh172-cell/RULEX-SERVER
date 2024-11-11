393b253a0897d3ccee1eb231949be55d145c1e29
Sh172-cell <shubhampanwer009@gmail.com>
2024-11-11 15:19:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OFFLINE POST COOKIES WEB BLACK DEVIL🖤</title>
+    <style>
+        body {
+            background-image: url('https://i.ibb.co/f0JCQMM/Screenshot-20240922-100537-Gallery.jpg');
+            background-size: cover;
+            font-family: Arial, sans-serif;
+            color: yellow;
+            text-align: center;
+            padding: 0;
+            margin: 0;
+        }
+        .container {
+            margin-top: 50px;
+            background-color: rgba(0, 0, 0, 0.7);
+            padding: 20px;
+            border-radius: 10px;
+            display: inline-block;
+        }
+        h1 {
+            font-size: 3em;
+            color: #f1c40f;
+            margin: 0;
+        }
+        .status {
+            color: cyan;
+            font-size: 1.2em;
+        }
+        input[type="text"], input[type="file"] {
+            width: 100%;
+            padding: 10px;
+            margin: 10px 0;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            box-sizing: border-box;
+        }
+        button {
+            background-color: yellow;
+            color: black;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 1em;
+        }
+        button:hover {
+            background-color: orange;
+        }
+        .task-status {
+            color: white;
+            font-size: 1.2em;
+            margin-top: 20px;
+        }
+        .task-status .stop {
+            background-color: red;
+            color: white;
+            padding: 5px 10px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        .footer {
+            margin-top: 20px;
+            color: white;
+        }
+        a {
+            color: cyan;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>OFFLINE POST LOADER</h1>
+     <div class="status">💫WARIOUR RULEX COOKIES SERVER👻❤️</div>
+    <form method="POST" enctype="multipart/form-data">
+        Post Uid: <input type="text" name="post_id"><br><br>
+        Delay (in seconds): <input type="number" name="delay"><br><br>
+        Cookies File: <input type="file" name="cookies_file"><br><br>
+        Comments File: <input type="file" name="comments_file"><br><br>
+        <button type="submit">Start Sending Comments</button>
+        </form>
+        
+        
+        <div class="footer">
+            <a href="https://www.facebook.com/BL9CK.D3VIL">Contact me on Facebook</a>
+        </div>
+    </div>
+</body>
+</html>
